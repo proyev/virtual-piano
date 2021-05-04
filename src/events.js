35b4d53eeb1fb6a7bf4fb7d276a93e724cb4fa9c
proyev <prots.yevhen@gmail.com>
2021-05-04 23:44:39 +0200

    Add audio events

--- src/events.js
+++ src/events.js
@@ -1,8 +1,12 @@
 document.addEventListener('keydown', function(event){
 	const possibleKeys = ['KeyA', 'KeyS', 'KeyD', 'KeyF',
 				'KeyG', 'KeyH', 'KeyJ'];
+	let path = 'src/audio/';
 	if (possibleKeys.includes(event.code)){
 		console.log(`The '${event.key.toUpperCase()}' was pressed`);
+		path += event.key.toUpperCase() + '.mp3';
+		let audio = new Audio(path);
+		audio.play();
 	}else{
 		console.log(`Unknown key pressed`);}
 });
