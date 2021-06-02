90bbcfb8fb70f99069f9c5e1516e25715d8dd616
proyev <prots.yevhen@gmail.com>
2021-06-02 23:59:42 +0200

    Add listeners for black keys

--- src/events.js
+++ src/events.js
@@ -1,12 +1,18 @@
-document.addEventListener('keydown', function(event){
-	const possibleKeys = ['KeyA', 'KeyS', 'KeyD', 'KeyF',
-				'KeyG', 'KeyH', 'KeyJ'];
-	let path = 'src/audio/';
-	if (possibleKeys.includes(event.code)){
-		console.log(`The '${event.key.toUpperCase()}' was pressed`);
-		path += event.key.toUpperCase() + '.mp3';
-		let audio = new Audio(path);
-		audio.play();
-	}else{
-		console.log(`Unknown key pressed`);}
+document.addEventListener('keydown', function (){
+   const possibleKeys = ['KeyA', 'KeyW',
+                        'KeyS', 'KeyE', 'KeyD',
+                        'KeyF', 'KeyT',
+                        'KeyG', 'KeyY',
+                        'KeyH', 'KeyU','KeyJ'];
+   let path = 'audio/';
+   if (possibleKeys.includes(event.code)){
+       console.log(`The '${possibleKeys[possibleKeys.indexOf(event.code)].slice(3)}' key is pressed.`);
+       path += event.key.toUpperCase() + '.mp3';
+       console.log(path);
+       let audio = new Audio(path);
+       audio.play();
+
+   }else{
+       console.log('Unknown key was pressed');
+   }
 });
